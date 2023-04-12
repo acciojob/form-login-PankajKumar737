61f8b2d4b180f0085bb4dead8cf7fe01c7058f1a
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-04-12 09:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	let fname = document.getElementByTagName("input").value;
+    let lname = document.getElementByTagName("input").value;
+	alert(fname + lname);
 
 }
